9d6016199054f1f770e4c34fb4b6f00a931cbd86
sujko123456 <matejsujansky@gmail.com>
2026-01-03 19:13:26 +0100

    Add React quiz project

---
+++ src/DateCounter.js
@@ -0,0 +1,73 @@
+// import { useReducer, useState } from "react";
+
+
+// function reducer( state,action){
+//   console.log(state,action);
+// }
+// function DateCounter() {
+//   // const [count, setCount] = useState(0);
+
+
+//   const initialState = { count:0, step:1};
+
+//   const [state, dispatch] = useReducer ( reducer , initialState);
+
+//   const {count,step} = state;
+
+//   // This mutates the date object.
+//   const date = new Date("june 21 2027");
+//   date.setDate(date.getDate() + count);
+
+//   const dec = function () {
+//     dispatch(1);
+
+//     // setCount((count) => count - 1);
+//     // setCount((count) => count - step);
+//   };
+
+//   const inc = function () {
+//     // setCount((count) => count + 1);
+//     // setCount((count) => count + step);
+//   };
+
+//   const defineCount = function (e) {
+//     // setCount(Number(e.target.value));
+//   };
+
+//   const defineStep = function (e) {
+//     setStep(Number(e.target.value));
+//   };
+
+//   const reset = function () {
+//     setCount(0);
+//     setStep(1);
+//   };
+
+//   return (
+//     <div className="counter">
+//       <div>
+//         <input
+//           type="range"
+//           min="0"
+//           max="10"
+//           value={step}
+//           onChange={defineStep}
+//         />
+//         <span>{step}</span>
+//       </div>
+
+//       <div>
+//         <button onClick={dec}>-</button>
+//         <input value={count} onChange={defineCount} />
+//         <button onClick={inc}>+</button>
+//       </div>
+
+//       <p>{date.toDateString()}</p>
+
+//       <div>
+//         <button onClick={reset}>Reset</button>
+//       </div>
+//     </div>
+//   );
+// }
+// export default DateCounter;
